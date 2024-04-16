d591898e6bc6aa0b959400dd642e9eb212945dde
SkyKnigth <129749991+SkyKnigth@users.noreply.github.com>
2024-04-16 20:07:11 +0200

    ajout code

--- FrontEnd/assets/js/modAdmin.js
+++ FrontEnd/assets/js/modAdmin.js
@@ -12,10 +12,11 @@ if (token) {
     });
     editMode.innerHTML=`<i class="fa-solid fa-pen-to-square"></i><span>modifier</span>`
     filters.classList.toggle("hidden")
-createEditionMode()
-editMode.addEventListener("click",function (event){
-genererModaleGalerie()
-})
+    createEditionMode()
+    editMode.addEventListener("click",function (event){
+        genererModaleGalerie()
+        console.log("ouvertureModal")
+    })
 }
 
 function createEditionMode() {
@@ -27,7 +28,6 @@ function createEditionMode() {
 
     headerEdition.innerHTML = logoEdition + modeEdition;
 
-
 }
 
 
@@ -36,14 +36,17 @@ function createEditionMode() {
 
 function genererModaleGalerie() {
     const modalContainer = document.getElementById("modalContainer");
+    modalContainer.classList.toggle("hidden");
 
-    const modal = document.getElementById("modal");
-    modal.style.display = "flex";
-    modal.innerHTML = "<div class=\"div_closeModal\"><button id=\"closeModal\"><i class=\"fa-solid fa-xmark\"></i></button></div>" +
+    const modalPartOne = document.getElementById("modal-part-one");
+    modalPartOne.style.display = "flex";
+    modalPartOne.innerHTML = "<div class=\"div_closeModal\"><button id=\"closeModal\"><i class=\"fa-solid fa-xmark\"></i></button></div>" +
         "<h3>Galerie Photo</h3>" +
         "<div class=\"presentation-images\"></div>" +
         "<button id=\"ajoutPhoto\">Ajouter une photo</button>" +
         "<button class=\"supprimer-galerie\">Supprimer la galerie</button>";
+    
+    genererModale()
 
     const closeModal = document.getElementById("closeModal");
    // closeModal.addEventListener("click", fermerModale);
@@ -52,7 +55,43 @@ function genererModaleGalerie() {
     //overlayModalTrigger.addEventListener("click", fermerModale);
 
     const ajoutPhoto = document.getElementById("ajoutPhoto");
-    /*ajoutPhoto.addEventListener("click", function () {
-        genererModaleAjout(works, categories);
-    });*/
+    ajoutPhoto.addEventListener("click", function () {
+        genererModaleAjout;
+    });
 }
+
+async function genererModale(){
+
+    const PartieModale = document.createElement("figure");
+    const presentationImage =document.querySelector(".presentation-images")
+    await getWorks()
+
+    for(let work of works){
+        console.log(work)
+        let figure = document.createElement("figure")
+        console.log(figure)
+        figure.setAttribute("data-categoryId",work.categoryId)
+        let figcaption = document.createElement("figcaption")
+        console.log(figcaption) 
+        figcaption.innerText= work.title
+        let image =document.createElement("img")
+        image.setAttribute("src",work.imageUrl)
+        image.setAttribute("alt",work.title)
+
+
+        figure.appendChild(image)
+        figure.appendChild(figcaption)
+        presentationImage.appendChild(figure)
+    }
+
+    const boutonMove = document.createElement("button");
+    boutonMove.innerHTML = "<i class=\"fa-solid fa-arrows-up-down-left-right\"></i>";
+   // boutonMove.id = "boutonMove";
+
+
+    const boutonSuppr = document.createElement("button");
+    boutonSuppr.innerHTML = "<i class=\"fa-solid fa-trash-can\"></i>";
+   // boutonSuppr.id = "boutonSuppr";
+
+}
+
