97763bea6606c99fd47dc4492af74c7ba97b924e
SkyKnigth <129749991+SkyKnigth@users.noreply.github.com>
2024-07-01 17:29:47 +0200

    integration modif

--- FrontEnd/assets/js/modAdmin.js
+++ FrontEnd/assets/js/modAdmin.js
@@ -23,7 +23,6 @@ if (token) {
 
 function createEditionMode() {
     const headerEdition = document.getElementById("header_edition");
-    headerEdition.style.display = "flex";
 
     const logoEdition = "<i class=\"fa-regular fa-pen-to-square\"></i>";
     const modeEdition = "<p>Mode édition</p>";
@@ -136,9 +135,49 @@ async function genererModale2(){
    // modalContainer.classList.toggle("hidden");
 
     //modalPartTwo.style.display = "block";
-    modalPartTwo.innerHTML = `<form></form>`
+    modalPartTwo.innerHTML = `<i id="button-Back" class="fa-solid fa-arrow-left"></i><div class="div_closeModal"><button id="closeModal"><i class="fa-solid fa-xmark"></i></button></div> <h3>Ajout Photo</h3> 
+    <form method="post" action="/" enctype="multipart/form-data">
+    <div class="icon">
+    <i class=""></i>
+    <label for="fileInput" class="file-label">
+    <input name="image" type="file" id="fileInput"/>
+    <span>+ Ajouter une Photo</span>
+</label>
+    <p>jpg png : 4mo max </p>
+    </div> 
+    <div>
+        <label for="title">titre</label>
+        <input id="title" name="title" type="text"/>
+    </div>
+    <select id="selectCategory">
+    </select>
+    </form>`
       
 
 }
 
 genererModale2()
+
+
+//const nouvelleImage = document.getElementById("IntegrateImage");
+const selectCategory = document.getElementById("selectCategory");
+async function generateSelect(){
+    await getCategories()
+    for(let category of categories){
+        console.log (category)
+        const option= document.createElement("option")
+        option.value=category.id
+        option.textContent=category.name
+        selectCategory.appendChild(option)
+       /* const button = document.createElement("button")
+        button.textContent=category.name
+        button.setAttribute("data-category",category.id)
+        button.addEventListener("click",function(event){
+            console.log(event.target)
+            const id=event.target.getAttribute("data-category")
+            Projects(id)
+        })
+        filters.appendChild(button)*/
+    }
+}
+generateSelect()
